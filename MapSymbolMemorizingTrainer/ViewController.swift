e1003d3520f11ac38b9c1c031dae8f02866d8d8c
Ryo Morimoto <morimoto@m-pc.local>
2016-01-05 22:07:32 +0900

    Initial Commit

---
+++ MapSymbolMemorizingTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapSymbolMemorizingTrainer
+//
+//  Created by Ryo Morimoto on 2016/01/05.
+//  Copyright © 2016年 Ryo Morimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
